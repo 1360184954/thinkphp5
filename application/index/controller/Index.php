c8fc62e7098b19fa34bf88affdedb7652042f22e
1360184954 <1360184954@qq.com>
2018-04-14 23:06:24 +0800

    登陆和注册页面添加，添加相关代码和JS

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,10 +6,11 @@ class Index extends Controller
 {
     public function index()
     {
-
         return $this->fetch();
-       //dump(111);die;
-        //return view('index');
+    }
 
+    public function register()
+    {
+        return $this->fetch();
     }
 }
--- public/js/login_js.js
+++ public/js/login_js.js
@@ -2,10 +2,5 @@
  * Created by q8982 on 2018/3/20.
  */
 $(function(){
-    $('.login-promptClose').click(function(){
-        $('.login-promptMesage').fadeOut(500);
-    });
-    $('.login-loginButton').click(function(){
-        $('.login-promptMesage').show(300);
-    });
+
 })
